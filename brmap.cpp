36f5ad2108877ed8168cc63c04d286509628a87f
root <root@kali>
2021-07-08 15:20:55 +0000

    Changes file names

---
+++ brmap.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <map>
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "brmap.h"
+
+using namespace std;
+
+
+
+  
+brmap::brmap() {}
+  
+void brmap::print() {
+    map<MACADDR,Bridge_entry>::iterator it = bridge.begin();
+    cout << "Bridge table (" << bridge.size() << ")\n";
+    for(; it != bridge.end(); it++)
+      {
+	it->first.print();
+	cout << " :: " << it->second.src_interface << endl;
+      }
+  }
+
+  /* Transparent learning bridge map
+     pkt_src    - source interface 
+     packet     - The packet to bridge
+     returns:   0: broadcast
+     1-n - dest interface
+     -1 - not found, drop
+  */
+int brmap::map_pkt(int pkt_src, unsigned char *packet) {
+
+    short dest_i = 0;
+    MACADDR src_mac,dest_mac;
+
+    // Get the addresses
+    src_mac.get_src_mac(packet);
+    dest_mac.get_dest_mac(packet);
+
+    /*  Process the source address.  Add if not in bridge. 
+	Update bridge if interface is different 
+	drop if broadcast from this interface */
+    if (bridge.find(src_mac) != bridge.end()) {
+      // check if changed 
+      Bridge_entry b = bridge.find(src_mac)->second;
+
+      int src_i = b.src_interface;
+      if (src_i != pkt_src) {
+	// update source
+	b.src_interface=pkt_src;
+	bridge[src_mac]=b;
+      }
+    } else { 
+      Bridge_entry new_entry(pkt_src);
+      bridge.emplace(src_mac,new_entry);
+    }
+    
+    if (dest_mac.is_multicast() || dest_mac.is_broadcast()) {
+      dest_i = 0;
+    } else {
+
+      /* find where to send packet in the bridge */
+      if (bridge.find(dest_mac) != bridge.end()) {
+	dest_i=bridge[dest_mac].src_interface;
+      } else {
+	// If we don't find the destination, just drop
+	dest_i=-1;
+      }
+    }
+    return dest_i;
+}
---
+++ brmap.h
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <map>
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+using namespace std;
+
+/* A mac address class */
+class MACADDR {
+  public:
+    unsigned char mac[6]={0};
+    MACADDR() {}
+    MACADDR(unsigned char a[6]) {
+      for (int i=0; i<6; i++) {
+        mac[i]=a[i];
+      }
+    }
+    
+    void set(unsigned char a[6]) {
+      for (int i=0; i<6; i++) {
+        mac[i]=a[i];
+      }
+    }
+    bool operator<(const MACADDR a) const {
+      for (int i=0; i<6; i++) {
+        if (a.mac[i] < this->mac[i])
+          return false;
+        if (a.mac[i] > this->mac[i])
+          return true;
+      }
+      return false;
+    }
+    bool operator=(const MACADDR a) const {
+      for (int i=0; i<6; i++) {
+        if (a.mac[i]!=this->mac[i])
+          return false;
+      }
+      return true;
+    }
+    void print() const {
+      cout << hex;
+      for (int i=0; i<6; i++) {
+        cout << (int) this->mac[i] << ' ';
+      }
+      cout << dec;
+    }
+    bool is_broadcast() {
+      for (int i=0; i<6; i++) {
+        if (mac[i]!=0xff) return false;
+      }
+      return true;
+    }
+
+    bool is_multicast() {
+
+      if (mac[0] & 0x01) {
+      return true;
+      }
+    return false;
+    }
+
+    void get_dest_mac(unsigned char *packet)   {
+        for (int i=0; i<6; i++) {
+          mac[i]=packet[i];
+        }
+    }
+
+    void get_src_mac(unsigned char *packet)   {
+        for (int i=0; i<6; i++) {
+          mac[i]=packet[i+6];
+        }
+    }
+
+  void random_mac() {
+    int i,tp;
+    for (i = 0; i < 6; i++) {
+      mac[i] = rand() % 256;
+    }
+  }
+};
+
+
+/* A bridge entry class */
+class Bridge_entry {
+  public:
+    short src_interface;
+    short vid;
+    MACADDR mac;
+    unsigned int ttl;
+    Bridge_entry(short src, short v) {
+      src_interface = src;
+      vid = v;
+    }
+    Bridge_entry(short src) {
+      src_interface = src;
+      vid = 0;
+    }
+    Bridge_entry() {
+      src_interface = 0;
+      vid = 0;
+    }
+
+};
+
+class brmap {
+
+ private:
+    map<MACADDR,Bridge_entry> bridge;
+
+ public:
+    
+    brmap(); // Constructor
+  
+    void print();
+
+    int map_pkt(int pkt_src, unsigned char *packet);
+
+};
