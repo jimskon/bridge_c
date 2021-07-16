b1f0a22c895c5618304d6787400c3cb8a6be92e7
Olivier Piras <github@oprs.eu>
2021-07-16 05:11:16 +0100

    only learn new MACs on broadcasts (enables WiFi)

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ ethtool -K eth0 tx off rx off gso off tso off gro off lro off
 ## Profiling support (gprof)
 
 Compile with: `make -j4 PROFILE=1`
+
 When ready, hit Ctrl+C to exit, then: `gprof bridge | less`
--- ifbridge.cpp
+++ ifbridge.cpp
@@ -253,22 +253,23 @@ ifbridge::fwd_upstream( pdu& pkt, iface *src )
 {
 	switch( pkt._sll.sll_pkttype ) {
 		case PACKET_BROADCAST:
+			{
+				macaddr addr( pkt._x + ETH_ALEN );
+				auto kv = _addr2if.find( addr );
+				if( kv == _addr2if.end() ) {
+					_log(2) << '[' << src->name() << "] add host " << addr << std::endl;
+					_addr2if.emplace( addr, src );
+					break;
+				}
+				// XXX also remember to update existing entries
+				// XXX (case of mobile devices roaming from one interface to another)
+			}
+			/*FALLTHROUGH*/
+
 		case PACKET_MULTICAST:
 		case PACKET_OTHERHOST:
-		if( pkt._sll.sll_protocol == ::htons( ETHERTYPE_ARP )) {
-			macaddr addr( pkt._x + ETH_ALEN );
-			auto kv = _addr2if.find( addr );
-			if( kv == _addr2if.end() ) {
-				_log(2) << '[' << src->name() << "] add host " << addr << std::endl;
-				_addr2if.emplace( addr, src );
-				break;
-			}
-			// XXX also remember to update existing entries
-			// XXX (case of mobile devices roaming from one interface to another)
-		}
-
-		_trunk.send( pkt );
-		break;
+			_trunk.send( pkt );
+			break;
 
 		default:
 			_log(3) << "unsupported packet type (upstream)" << std::endl;
